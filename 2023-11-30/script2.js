fccc29f03afe6eae964e71548cb78b6089db7f44
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-11-30 20:37:01 +0200

    save

--- 2023-11-30/script2.js
+++ 2023-11-30/script2.js
@@ -16,7 +16,7 @@ if (sk >= 0 && sk <= 5) {
 } else if (sk >= 16 && sk <= 30 && sk % 5 === 0) {
   let rez = sk + 10;
   console.log(
-    `Sugeneruotas skaicius ${sk}. Tracia kategorija. Sugeneruotas skaicius + 10 = ${rez}.`
+    `Sugeneruotas skaicius ${sk}. Trecia kategorija. Sugeneruotas skaicius + 10 = ${rez}.`
   );
 } else if (sk >= 31 && sk <= 45 && sk % 2 !== 0) {
   let rez = 100 - sk;
@@ -24,13 +24,12 @@ if (sk >= 0 && sk <= 5) {
     `Sugeneruotas skaicius ${sk}. Ketvirta kategorija. 100 - sugeneruotas skaicius = ${rez}.`
   );
 }
-else if (sk >= 46 && sk <= 99 && sk === 47) {
-
+else if (sk === 46 || sk === 47 || sk === 56 || sk === 57 || sk === 66 || sk === 67|| sk === 76  || sk ===77 || sk ===86 || sk === 87 || sk === 96 || sk === 97) {
 let rez = sk / 4;
 console.log(
     `Sugeneruotas skaicius ${sk}. Penkta kategorija. Sugeneruotas skaicius / 4 = ${rez}.`
   );
 }
 else {
-    console.log(`Skaicius nepatenka i kategorijas!`)
+    console.log(`Sugeneruotas skaicius nepatenka i kategorijas!`)
 }
