5d57666fa167924683d2c5aa0b495617ea982bcf
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-12-02 15:57:03 +0200

    save

---
+++ 2023-12-01/script2.js
@@ -0,0 +1,25 @@
+function calc() {
+  let skaicius1 = parseInt(document.getElementById("skaicius1").value);
+  let skaicius2 = parseInt(document.getElementById("skaicius2").value);
+  let operacijos = document.getElementById("operacijos").value;
+
+  if (operacijos === "+") {
+    document.getElementById("rezultatas").value = skaicius1 + skaicius2;
+  }
+  if (operacijos === "-") {
+    document.getElementById("rezultatas").value = skaicius1 - skaicius2;
+  }
+  if (operacijos === "x") {
+    document.getElementById("rezultatas").value = skaicius1 * skaicius2;
+  }
+  if (operacijos === "/") {
+    document.getElementById("rezultatas").value = skaicius1 / skaicius2;
+  }
+  if (operacijos === "x²") {
+    document.getElementById("rezultatas").value = skaicius1 ** skaicius2;
+  }
+  if (operacijos === "/" && skaicius2 === 0) {
+    document.getElementById("rezultatas").value = "VEIKSMAS NEGALIMAS";
+  }
+}
+
