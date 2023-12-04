21eb679a313553f653975355e81cc57430c40278
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-12-04 15:03:49 +0200

    save

--- 2023-12-04/script.js
+++ 2023-12-04/script.js
@@ -1,33 +1,154 @@
 let n = 1;
-let simbolis = `<black>*</black>`, redSimbolis = `<red>*</red>`;
+let simbolis = `<black>*</black>`,
+  redSimbolis = `<red>*</red>`;
 
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = redSimbolis  + simbolis+ simbolis + simbolis + simbolis + simbolis + simbolis+ simbolis + simbolis + redSimbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + redSimbolis+ simbolis + simbolis + simbolis + simbolis + simbolis+ simbolis + redSimbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + simbolis+ redSimbolis + simbolis + simbolis + simbolis + simbolis+ redSimbolis + simbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + simbolis+ simbolis + redSimbolis + simbolis + simbolis + redSimbolis+ simbolis + simbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + simbolis+ simbolis + simbolis + redSimbolis + redSimbolis + simbolis+ simbolis + simbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + simbolis+ simbolis + simbolis + redSimbolis + redSimbolis + simbolis+ simbolis + simbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + simbolis+ simbolis + redSimbolis + simbolis + simbolis + redSimbolis+ simbolis + simbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + simbolis+ redSimbolis + simbolis + simbolis + simbolis + simbolis+ redSimbolis + simbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = simbolis  + redSimbolis+ simbolis + simbolis + simbolis + simbolis + simbolis+ simbolis + redSimbolis + simbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    simbolis +
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis +
+    simbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
-for (let indeksas = 0; indeksas < n; indeksas++) {let eilute = redSimbolis  + simbolis+ simbolis + simbolis + simbolis + simbolis + simbolis+ simbolis + simbolis + redSimbolis; eilute += "<br>"
-    document.write(eilute);
+for (let indeksas = 0; indeksas < n; indeksas++) {
+  let eilute =
+    redSimbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    simbolis +
+    redSimbolis;
+  eilute += "<br>";
+  document.write(eilute);
 }
