675da8badff1a0c308cf7b3302f7540b4194c8f9
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-12-09 15:04:50 +0200

    save

--- 2023-11-29/pasikartojimas.js
+++ 2023-11-29/pasikartojimas.js
@@ -1,3 +1,7 @@
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 let vardas = "Mantas",
   pavarde = "Savickas";
 if (vardas.length > pavarde.length) console.log(vardas);
@@ -22,37 +26,74 @@ let menesis = data[5] + data[6];
 
 if (data[5] + data[6] === "01") {
   console.log("Sausis");
-}
-else if  (data[5] + data[6] === "02") {
+} else if (menesis === "02") {
   console.log("Vasaris");
-}
-else if (data[5] + data[6] === "03") {
+} else if (menesis === "03") {
   console.log("Kovas");
-}
-else if (data[5] + data[6] === "04") {
+} else if (menesis === "04") {
   console.log("Balandis");
-}
-else if (data[5] + data[6] === "05") {
+} else if (menesis === "05") {
   console.log("Geguze");
-}
-else if (data[5] + data[6] === "06") {
+} else if (menesis === "06") {
   console.log("Birzelis");
-}
-else if (data[5] + data[6] === "07") {
+} else if (menesis === "07") {
   console.log("Liepa");
-}
-else if (data[5] + data[6] === "08") {
+} else if (menesis === "08") {
   console.log("Rugpjutis");
-}
-else if (data[5] + data[6] === "09") {
+} else if (menesis === "09") {
   console.log("Rugsejis");
-}
-else if (data[5] + data[6] === "10") {
+} else if (menesis === "10") {
   console.log("Spalis");
-}
-else if (data[5] + data[6] === "11") {
+} else if (menesis === "11") {
   console.log("Lapkritis");
-}
-else if (data[5] + data[6] === "12") {
+} else if (menesis === "12") {
   console.log("Gruodis");
 } else console.log("TOKIO MENESIO NERA!");
+
+let tekstas = "Once upon a time in hollywood.",
+  mazosiosRaides = tekstas.toLocaleLowerCase(),
+  zvaigzdutes = mazosiosRaides.replaceAll("o", "*");
+console.log(zvaigzdutes);
+
+let sk1 = rand(0, 2),
+  sk2 = rand(0, 2),
+  sk3 = rand(0, 2),
+  sk4 = rand(0, 2);
+let nuliai = 0,
+  vienetai = 0,
+  dvejetai = 0;
+
+if (sk1 === 0) nuliai++;
+else if (sk1 === 1) vienetai++;
+else dvejetai++;
+if (sk2 === 0) nuliai++;
+else if (sk2 === 1) vienetai++;
+else dvejetai++;
+if (sk3 === 0) nuliai++;
+else if (sk3 === 1) vienetai++;
+else dvejetai++;
+if (sk4 === 0) nuliai++;
+else if (sk4 === 1) vienetai++;
+else dvejetai++;
+console.log(`Nuliu - ${nuliai}, Vienetu - ${vienetai}, Dvejetu - ${dvejetai}.`);
+
+let n1 = rand(0, 4),
+  n2 = rand(0, 4);
+rez = 0;
+
+
+if (n1 > n2) {
+  rez = n1 / n2;
+} else if (n1 < n2) {
+  rez = n2 / n1;
+}
+
+if (n1 === n2) {
+  rez = console.log("Kintamieji yra lygus.");
+}
+if (n2 === 0) {
+  rez = console.log("Dalyba is nulio negalima!");
+} else if (n1 === 0) {
+  rez = console.log("Dalyba is nulio negalima!");
+}
+console.log(rez.toFixed(2));
