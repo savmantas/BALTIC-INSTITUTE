3b84a9636415522078a5958784c655dd3cdc1a1f
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-12-10 19:01:04 +0200

    save

--- 2023-12-08/namuDarbas.js
+++ 2023-12-08/namuDarbas.js
@@ -1,11 +1,10 @@
 let masyvas = new Array(200);
 for (let i = 0; i < masyvas.length; i++) {
-    let atsitiktineRaide = String.fromCharCode(
-        65 + Math.floor(Math.random() * 4)
-        );
-        masyvas[i] = atsitiktineRaide;
-    }
-
+  let atsitiktineRaide = String.fromCharCode(
+    65 + Math.floor(Math.random() * 4)
+  );
+  masyvas[i] = atsitiktineRaide;
+}
 
 let countA = masyvas.filter((raide) => raide === "A").length,
   countB = masyvas.filter((raide) => raide === "B").length,
@@ -17,9 +16,30 @@ console.log("B raides count: " + countB);
 console.log("C raides count: " + countC);
 console.log("D raides count: " + countD);
 
-masyvas.sort(function(a, b) {
-
-    return a.localeCompare(b);
+masyvas.sort(function (a, b) {
+  return a.localeCompare(b);
 });
 
-console.log('Masyvas pagal abecele:', masyvas);
+console.log("Masyvas pagal abecele:", masyvas);
+
+function generuotiUnikaliusSkaicius(min, max, naudoti) {
+  let skaicius;
+  do {
+    skaicius = Math.floor(Math.random() * (max - min + 1)) + min;
+  } while (naudoti.includes(skaicius));
+  return skaicius;
+}
+
+let masyvas1 = [],
+  masyvas2 = [];
+
+for (let i = 0; i < 100; i++) {
+  let unikalusSkaiciusMasyvo1 = generuotiUnikaliusSkaicius(100, 999, masyvas1);
+  let unikalusSkaiciusMasyvo2 = generuotiUnikaliusSkaicius(100, 999, masyvas2);
+
+  masyvas1.push(unikalusSkaiciusMasyvo1);
+  masyvas2.push(unikalusSkaiciusMasyvo2);
+}
+
+console.log("Masyvas 1:", masyvas1);
+console.log("Masyvas 2:", masyvas2);
