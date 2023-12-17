ba8c1981f42df29429797238295e06a116201889
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-12-17 13:55:45 +0200

    save

--- 2023-11-14/mokausi.js
+++ 2023-11-14/mokausi.js
@@ -2,3 +2,12 @@ function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function categoryOneCalculation(skaicius){
+  let rez = skaicius **2;
+  return rez;
+}
+
+let sk = rand(0,99)
+if (sk<=10){
+  console.log(`Skaicius: ${sk}, Kategorija: 1, Rezultatas ${categoryOneCalculation(sk)}`);
+}
--- 2023-11-30/script2.js
+++ 2023-11-30/script2.js
@@ -1,45 +1,61 @@
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function categoryOneCalculation(skaicius) {
+  let rez = skaicius * 3;
+  return rez;
+}
+
+function categoryTwoCalculation(skaicius) {
+  let rez = (skaicius - 4) ** 2;
+  return rez;
+}
+
+function categoryThirdCalculation(skaicius) {
+  let rez = skaicius + 10;
+  return rez;
+}
+function categoryFourthCalculation(skaicius) {
+  let rez = 100 - skaicius;
+  return rez;
+}
+function categoryFifthCalculation(skaicius) {
+  let rez = skaicius / 4;
+  return rez;
+}
+
 let sk = rand(0, 99);
 
+console.log(sk);
 if (sk >= 0 && sk <= 5) {
-  let rez = sk * 3;
   console.log(
-    `Sugeneruotas skaicius ${sk}. Pirma kategorija. Sugeneruotas skaicius * 3 = ${rez}.`
+    `Sugeneruotas skaicius ${sk}. Pirma kategorija. Sugeneruotas skaicius ${categoryOneCalculation(
+      sk
+    )}.`
   );
 } else if (sk >= 6 && sk <= 15) {
-  let rez = (sk - 4) ** 2;
   console.log(
-    `Sugeneruotas skaicius ${sk}. Antra kategorija. (Sugeneruotas skaicius - 4) ** 2 = ${rez}.`
+    `Sugeneruotas skaicius ${sk}. Antra kategorija. (Sugeneruotas skaicius - 4) ** 2 = ${categoryTwoCalculation(
+      sk
+    )}.`
   );
 } else if (sk >= 16 && sk <= 30 && sk % 5 === 0) {
-  let rez = sk + 10;
   console.log(
-    `Sugeneruotas skaicius ${sk}. Trecia kategorija. Sugeneruotas skaicius + 10 = ${rez}.`
+    `Sugeneruotas skaicius ${sk}. Trecia kategorija. Sugeneruotas skaicius + 10 = ${categoryThirdCalculation(
+      sk
+    )}.`
   );
 } else if (sk >= 31 && sk <= 45 && sk % 2 !== 0) {
-  let rez = 100 - sk;
   console.log(
-    `Sugeneruotas skaicius ${sk}. Ketvirta kategorija. 100 - sugeneruotas skaicius = ${rez}.`
+    `Sugeneruotas skaicius ${sk}. Ketvirta kategorija. 100 - sugeneruotas skaicius = ${categoryFourthCalculation(
+      sk
+    )}.`
   );
-} else if (
-  sk === 46 ||
-  sk === 47 ||
-  sk === 56 ||
-  sk === 57 ||
-  sk === 66 ||
-  sk === 67 ||
-  sk === 76 ||
-  sk === 77 ||
-  sk === 86 ||
-  sk === 87 ||
-  sk === 96 ||
-  sk === 97
-) {
-  let rez = sk / 4;
+} else if ((sk >= 46 && sk <= 99 && sk % 10 === 6) || sk % 10 === 7) {
   console.log(
-    `Sugeneruotas skaicius ${sk}. Penkta kategorija. Sugeneruotas skaicius / 4 = ${rez}.`
+    `Sugeneruotas skaicius ${sk}. Penkta kategorija. Sugeneruotas skaicius / 4 = ${categoryFifthCalculation(
+      sk
+    )}.`
   );
 } else {
   console.log(`Sugeneruotas skaicius nepatenka i kategorijas!`);
@@ -58,13 +74,14 @@ function login() {
   let prisijungimoSlaptazodzioIvestis = document.querySelector(
     "#prisijungimoSlaptazodis"
   );
- let  prisijungimoVardas = prisijungimoVardoIvestis.value
- let prisijungimoSlaptazodis = prisijungimoSlaptazodzioIvestis.value
- if (loginName === prisijungimoVardas && password === prisijungimoSlaptazodis)
- {
-document.write('Prisijungete sekmingai.')
- }
- else{
-document.write('Prisijungimo duomenys yra blogi.')
- }
+  let prisijungimoVardas = prisijungimoVardoIvestis.value;
+  let prisijungimoSlaptazodis = prisijungimoSlaptazodzioIvestis.value;
+  if (
+    loginName === prisijungimoVardas &&
+    password === prisijungimoSlaptazodis
+  ) {
+    document.write("Prisijungete sekmingai.");
+  } else {
+    document.write("Prisijungimo duomenys yra blogi.");
+  }
 }
