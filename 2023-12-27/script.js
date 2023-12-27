6a7828b64e32f98e6cd63bdc328a005620eb130d
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2023-12-27 15:54:24 +0200

    save

---
+++ 2023-12-27/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((atsakymas) => atsakymas.json())
+    .then((info) => {
+      const breedsSelect = document.getElementById("breeds");
+      const breeds = Object.keys(info.message);
+      breeds.forEach((breed) => {
+        const option = document.createElement("option");
+        option.value = breed;
+        option.textContent = breed;
+        breedsSelect.appendChild(option);
+      });
+      image();
+    });
+  window.image = function () {
+    const selectedBreed = document.getElementById("breeds").value;
+    const dogImage = document.getElementById("dogImage");
+    fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random`)
+      .then((atsakymas) => atsakymas.json())
+      .then((info) => {
+        const img = document.createElement("img");
+        img.src = info.message;
+        dogImage.innerHTML = "";
+        dogImage.appendChild(img);
+      });
+  };
+});
