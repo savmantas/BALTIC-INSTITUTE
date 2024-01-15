259190581ff15dbadf29e4378d3a9f3686838461
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2024-01-15 12:57:52 +0200

    save

--- 2023/2023-12-12/dynamicContent.js
+++ 2023/2023-12-12/dynamicContent.js
@@ -1,49 +1,50 @@
 const people = [];
 let currentNumeration = 1;
 
-const buttonElement = document.querySelector("#add-button");
-
-buttonElement.addEventListener("click", () => {
+const addButtonElement = document.querySelector("#add-button");
+addButtonElement.addEventListener("click", () => {
   const person = {};
-
   person.firstName = document.getElementById("firstNameInput").value;
   person.lastName = document.getElementById("lastNameInput").value;
   person.age = document.getElementById("ageInput").value;
   person.nationality = document.getElementById("nationalityInput").value;
   person.number = currentNumeration;
-
-  people.push(person);
-
   currentNumeration++;
-
+  people.push(person);
   generateTableContent(people);
 });
-removeButton.addEventListener("click", () => {
-    const removeNumber = parseInt(document.getElementById("removeNumberInput").value);
 
-    const indexToRemove = people.findIndex(person => person.number === removeNumber);
+const deleteButtonElement = document.querySelector("#delete-button");
+deleteButtonElement.addEventListener("click", () => {
+  const person = {};
+  person.firstName =''
+  person.lastName =''
+  person.age =''
+  person.nationality =''
+  person.number = currentNumeration;
+  currentNumeration++;
+  people.push(person);
+  generateTableContent(people);
+});
 
-    if (indexToRemove !== -1) {
-      people.splice(indexToRemove, 1);
-      generateTableContent(people);
-    } else {
-      alert('Person with the specified number does not exist.');
-    }
-  });
 function generateTableContent(people) {
   let dynamicHTML = ``;
-
   for (let person of people) {
     dynamicHTML += `<tr>
-        <td>${person.number}</td>
-        <td>${person.firstName}</td>
-        <td>${person.lastName}</td>
-        <td>${person.age}</td>
-        <td>${person.nationality}</td>
-        </tr>`;
+<td>${person.number}</td>
+<td>${person.firstName}</td>
+<td>${person.lastName}</td>
+<td>${person.age}</td>
+<td>${person.nationality}</td>
+</tr>`;
   }
-
   const tbody = document.querySelector("table tbody");
-
   tbody.innerHTML = dynamicHTML;
+}
+
+function updateTableContent(people) {
+  person.firstName = document.getElementById("firstNameInput").value;
+  person.lastName = document.getElementById("lastNameInput").value;
+  person.age = document.getElementById("ageInput").value;
+  person.nationality = document.getElementById("nationalityInput").value;
 }
