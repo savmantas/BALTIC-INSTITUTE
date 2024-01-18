20f3ace07405b72483eeabf200f0a32942f03769
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2024-01-18 16:10:35 +0200

    save

--- DEV/JS practice/scriptas.js
+++ DEV/JS practice/scriptas.js
@@ -8,3 +8,6 @@
 // );
 // console.log((writtenCharacters.slice(0,100)));
 
+let dogAge = prompt("How old is your dog?");
+let humanAge = (dogAge - 2) * 4 + 21;
+alert("Your dog is " + humanAge + "years old in human years.");
