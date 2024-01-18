9d1248c6bd02b5f25d92a45877b77d79ad3526b8
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2024-01-18 14:03:10 +0200

    save

--- DEV/JS practice/scriptas.js
+++ DEV/JS practice/scriptas.js
@@ -1,9 +1,10 @@
-let availableCharacters = 100;
-let writtenCharacters =
-  "The Weather Channel and weather.com provide a national and local weather forecast for cities, as well as weather radar, report and hurricane coverage.";
-let leftToWriteCharacters = availableCharacters - writtenCharacters.length;
+// let availableCharacters = 100;
+// let writtenCharacters =
+//   "The Weather Channel and weather.com provide a national and local weather forecast for cities, as well as weather radar, report and hurricane coverage.";
+// let leftToWriteCharacters = availableCharacters - writtenCharacters.length;
 
-console.log(
-  `You have written ${writtenCharacters.length} characters, you have left ${leftToWriteCharacters} characters left.`
-);
-console.log((writtenCharacters.slice(0,100)));
+// console.log(
+//   `You have written ${writtenCharacters.length} characters, you have left ${leftToWriteCharacters} characters left.`
+// );
+// console.log((writtenCharacters.slice(0,100)));
+
