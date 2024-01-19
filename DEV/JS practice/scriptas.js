817eda2de1542eecf3a26fd481af62692436c9de
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2024-01-19 08:06:46 +0200

    save

--- DEV/JS practice/scriptas.js
+++ DEV/JS practice/scriptas.js
@@ -2,12 +2,18 @@
 // let writtenCharacters =
 //   "The Weather Channel and weather.com provide a national and local weather forecast for cities, as well as weather radar, report and hurricane coverage.";
 // let leftToWriteCharacters = availableCharacters - writtenCharacters.length;
-
 // console.log(
 //   `You have written ${writtenCharacters.length} characters, you have left ${leftToWriteCharacters} characters left.`
 // );
 // console.log((writtenCharacters.slice(0,100)));
 
-let dogAge = prompt("How old is your dog?");
-let humanAge = (dogAge - 2) * 4 + 21;
-alert("Your dog is " + humanAge + "years old in human years.");
+
+
+// let dogAge = prompt("How old is your dog?");
+// let humanAge = (dogAge - 2) * 4 + 21;
+// alert("Your dog is " + humanAge + "years old in human years.");
+
+
+
+
+
