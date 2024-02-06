2092ffbb07346811f033f8098b53db9a75f95818
Mantas Savickas <149368306+savmantas@users.noreply.github.com>
2024-02-06 14:20:10 +0200

    save

---
+++ 2023/MOKAUSI/script.js
@@ -0,0 +1,105 @@
+//-------------------------FUNCTIONS----------------------
+
+// function fruitProcessor(apples, oranges){
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice;
+// }
+// const appleJuice = fruitProcessor(5,0)
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitProcessor(2,3)
+// console.log(appleOrangeJuice);
+
+//---------------------------------------------------------
+
+// function calcAge1(birthYear) {
+//   return 2024 - birthYear;
+// }
+
+// const age1 = calcAge1(1987);
+// console.log(age1);
+
+// const calcAge2 = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+// const age2 = calcAge2(1987);
+
+// console.log(age1, age2);
+
+//-------------------------------------------------------
+
+// const calcAge3 = (birthYear) => 2024 - birthYear;
+// const age3 = calcAge3(1987);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2024 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+// console.log(yearsUntilRetirement(1987, 'Mantas'));
+// console.log(yearsUntilRetirement(1992, 'Tomas'));
+
+//-------------------------------------------------------
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+//-------------------------------------------------------
+
+// const calAge = function (year) {
+//   return 2024 - year;
+// };
+// function yearsUntilRetirement(birthYear, firstName) {
+//   const age = calAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//       console.log(`${firstName} retires in ${retirement} years.`);
+//       return retirement;
+//   } else {
+//       console.log(`${firstName} has already retired.`);
+//       return -1;
+//   }
+// }
+
+// console.log(yearsUntilRetirement(1987, "Mantas"));
+// console.log(yearsUntilRetirement(1950, "Tomas"));
+
+//-------------------------------------------------------
+
+// function calcAverage(score1, score2, score3) {
+//   const average = (score1 + score2 + score3) / 3;
+//   return average;
+// }
+
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+
+// function checkWinner(avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     return "Dolphins win!";
+//   } else if (avgDolphins * 2 <= avgKoalas) {
+//     return "Koalas win!";
+//   } else {
+//     return "No team wins...!";
+//   }
+// }
+// console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+//------------------------Arrays------------------------
+
+const friends = ["Mantas", "Tomas", "Audrius"];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length -1]);
